f696d42516cc9178f7e6664e915fd32ccfd87a63
Rita <marharitavol@gmail.com>
2023-03-17 18:39:06 +0200

    Created: - Character List View - Character Cell

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -15,8 +15,45 @@ final class RMService {
     /// Privatized constructor
     private init() {}
     
+    enum RMServiceError: Error {
+        case failedToCreateRequest
+        case failedToGetData
+    }
     
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+    public func execute<T: Codable>(_ request: RMRequest, expepecting type: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
         
+        guard let urlRequest = self.request(from: request) else {
+            completion(.failure(RMServiceError.failedToCreateRequest))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? RMServiceError.failedToGetData))
+                return
+            }
+            
+            // Decode response
+            do {
+                let json = try JSONSerialization.jsonObject(with: data)
+                print(json)
+                
+                let result = try JSONDecoder().decode(type.self, from: data)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+    // MARK: - Private
+    private func request(from rmRequest: RMRequest) -> URLRequest? {
+        guard let url = rmRequest.url else { return nil }
+        print(url)
+        var request = URLRequest(url: url)
+        request.httpMethod = rmRequest.httpMethod
+        
+        return request
     }
 }
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 final class RMCharacterViewController: UIViewController {
-
+    
+    private let characterListView = RMCharacterListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
+        view.addSubview(characterListView)
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            
+        ])
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
