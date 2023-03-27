3661e7a9becfce34ab6904e621fb5afc26e45f85
Rita <marharitavol@gmail.com>
2023-03-27 14:47:47 +0300

    Fetch Episodes

--- RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
+++ RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
@@ -13,6 +13,8 @@ class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.backgroundColor = .systemBlue
+        contentView.layer.cornerRadius = 8
     }
     
     required init?(coder: NSCoder) {
@@ -28,6 +30,10 @@ class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     }
     
     public func configure(with viewModel: RMCharacterEpisodeCollectionViewCellViewModel) {
+        viewModel.registerForData { data in
+            print(data.name)
+        }
+        viewModel.fetchEpisode()
         
     }
 }
