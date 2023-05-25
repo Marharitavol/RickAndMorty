053de5ec2d8fbfd169db5313e870bb70c657d6e4
Rita <marharitavol@gmail.com>
2023-05-25 23:14:11 +0300

    Begin Location Tab

--- RickAndMorty/Controllers/Core/RMLocationViewController.swift
+++ RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -8,22 +8,39 @@
 import UIKit
 
 final class RMLocationViewController: UIViewController {
-
+    
+    private let primaryView = RMLocationView()
+    
+    private let viewModel = RMLocationViewViewModel()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubviews(primaryView)
         view.backgroundColor = .systemBackground
         title = "Locations"
+        addSearchButton()
+        addConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func addSearchButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(didTapSearch))
     }
-    */
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+           primaryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+           primaryView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+           primaryView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+           primaryView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    @objc private func didTapSearch() {
+//        let vc = RMSearchViewController(config: RMSearchViewController.Config(type: .character))
+//        vc.navigationItem.largeTitleDisplayMode = .never
+//        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
---
+++ RickAndMorty/Views/Location/RMLocationView.swift
@@ -0,0 +1,72 @@
+//
+//  RMLocationView.swift
+//  RickAndMorty
+//
+//  Created by Rita on 25.05.2023.
+//
+
+import UIKit
+
+final class RMLocationView: UIView {
+    
+    private var viewModel: RMLocationViewViewModel? {
+        didSet {
+            spinner.startAnimating()
+            tableView.isHidden = false
+            tableView.reloadData()
+            UIView.animate(withDuration: 0) {
+                self.tableView.alpha = 1
+            }
+        }
+    }
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        
+        return spinner
+    }()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubviews(tableView, spinner)
+        spinner.startAnimating()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+        ])
+    }
+    
+    public func configure(with viewModel: RMLocationViewViewModel) {
+        self.viewModel = viewModel
+    }
+}
