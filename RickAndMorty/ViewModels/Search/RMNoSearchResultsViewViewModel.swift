7152e3e8275df321778d042c88956ae58a758aa5
Rita <marharitavol@gmail.com>
2023-10-02 13:43:12 +0300

    Added No Search Results View

--- RickAndMorty/ViewModels/Search/RMNoSearchResultsViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMNoSearchResultsViewViewModel.swift
@@ -5,8 +5,9 @@
 //  Created by Rita on 30.09.2023.
 //
 
-import Foundation
+import UIKit
 
 struct RMNoSearchResultsViewViewModel {
-    
+    let title = "No Results"
+    let image = UIImage(systemName: "magnifyingglass.circle")
 }
