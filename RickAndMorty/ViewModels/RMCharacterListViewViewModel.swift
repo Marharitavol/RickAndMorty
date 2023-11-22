21cd7adb9daff20684aaea904c3d27af7cd0099e
Rita <marharitavol@gmail.com>
2023-11-22 13:31:24 +0200

    Hook up character and Episode searching

--- RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
@@ -146,7 +146,6 @@ extension RMCharacterListViewViewModel:  UICollectionViewDataSource, UICollectio
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let bounds = UIScreen.main.bounds
-        
         let width = (bounds.width-30)/2
         return CGSize(
             width: width,
